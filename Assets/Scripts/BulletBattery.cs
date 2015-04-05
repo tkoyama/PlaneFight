b67e9a25383c0a35d80c486c69829b8438dcb287
tkoyama <tkoyama@aiming-inc.com>
2015-04-06 01:48:22 +0900

    bulletを打てる

---
+++ Assets/Scripts/BulletBattery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBattery : MonoBehaviour {
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1")) {
+		Debug.Log ("fire");
+			Instantiate(bullet, transform.position, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour {
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward.normalized * speed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
